d0b52e1198e816dc3762a826d48d798b36ecc86a
Brett Lyons <blyons@students.naropa.edu>
2016-01-09 19:53:49 -0700

    Added some .css, delete button now working

--- src-cljs/capstone_cljs_front_end/core.cljs
+++ src-cljs/capstone_cljs_front_end/core.cljs
@@ -50,30 +50,27 @@
 (defonce test-info (reagent/atom {}))
 
 (defn minus-api-info [id]
-  (swap! test-info dissoc id)) 
+  (swap! test-info update-in [:testing] dissoc id)) 
 
-(defn getTest [id]
-  (get @test-info id))
+(defn get-test [id]
+  (get-in @test-info [:testing id]))
 
-(defn getName [id]
-  (get-in @test-info [id :name]))
-
-(defn add-api-test! []
-    (swap! test-info assoc-in [:testing id] {:id id
-                                             :address nil 
-                                             :name nil
-                                             :payload nil
-                                             :action nil}))
+(defn get-testing-name [id]
+  (get-in @test-info [:testing id :name]))
 
 (defn set-editing! [& test-id]
+  ;; (println "test-id passed into set-editing! : " (first test-id) "get-test: " (get-test (first test-id)))
+  (if (nil? test-id)
+    (swap! test-info assoc :editing {:address nil 
+                                     :name nil
+                                     :payload nil
+                                     :action nil})
+    (swap! test-info assoc :editing (get-test (first test-id)))))
+
+(defn add-api-test! []
   (let [id (swap! counter inc)]
-    (if (nil? test-id)
-      (swap! test-info assoc :editing {:id id
-                                       :address nil 
-                                       :name nil
-                                       :payload nil
-                                       :action nil})
-      (swap! test-info assoc :editing (getTest test-id)))))
+    (swap! test-info assoc-in [:testing id] (:editing @test-info))
+    (set-editing!)))
 
 
 (defonce init (do
@@ -107,24 +104,24 @@
     [:span.glyphicon.glyphicon-plus] (str " Api To Test List")]])
 
 (defn rm-test-btn [id]
-  [:button.btn.btn-default.btn-md.pull-right {:on-click #(minus-api-info id)}
-   [:span.glyphicon.glyphicon-remove]])
+  [:button.destroy.pull-right {:on-click #(minus-api-info id)}])
 
 (defn api-form []
   ;; (println "ID: " id)
-  (let [id (:editing @test-info)]
+  (let [id (:id (:editing @test-info))]
     (fn []
       [:div.row
       [:p]
        [:div.col-md-8
-        [:div.btn-group {:role "group" :id (str "action-" id)}
+        [:div.btn-group {:role "group" :id (str "action-" (:editing @test-info))}
          [:button.btn.btn-default {:type "button"
                                    :name "get-btn"
                                    :value "GET"
-                                   :on-click #(swap! test-info assoc-in [id :action] (.-target.value %)
-                                                     ())} "GET"]
+                                   :on-click #(swap! test-info assoc-in [:editing :action] (.-target.value %))} "GET"]
 
-         [:button.btn.btn-default {:type "button" :value "POST" :on-click #(swap! test-info assoc-in [id :action] (.-target.value %))} "POST"]]
+         [:button.btn.btn-default {:type "button"
+                                   :value "POST"
+                                   :on-click #(swap! test-info assoc-in [:editing :action] (.-target.value %))} "POST"]]
         
         ;;[rm-test-btn id]
         [:p]
@@ -132,36 +129,36 @@
         [:div.input-group
          [:span.input-group-addon "Url: "]
          [:input.form-control {:type "URL"
-                               :value (get-in @test-info [id :address])
-                               :on-change #(swap! test-info assoc-in [id :address] (.-target.value %))}]]
+                               :value (get-in @test-info [:editing :address])
+                               :on-change #(swap! test-info assoc-in [:editing :address] (.-target.value %))}]]
 
 
         [:label "Name of the API to appear on the graph"]
         [:div.input-group
          [:span.input-group-addon "Name: "]
          [:input.form-control {:type "text"
-                               :value (get-in @test-info [id :name])
-                               :on-change #(swap! test-info assoc-in [id :name] (.-target.value %))}]]]
+                               :value (get-in @test-info [:editing :name])
+                               :on-change #(swap! test-info assoc-in [:editing :name] (.-target.value %))}]]]
        [:div.col-md-4
         [:p]
         [:div {:style {:visibility ((fn []
-                                      (if (= (get-in @test-info [id :action]) "POST")
+                                      (if (= (get-in @test-info [:editing :action]) "POST")
                                         "visible"
                                         "hidden"))) }}
          [:label "Payload for POST"]
          [:div.input-group 
           [:textarea.form-control {:type "textarea"
                                    :rows 5
-                                   :value (get-in @test-info [id :payload])
-                                   :on-change #(swap! test-info assoc-in  [id :payload] (.-target.value %))}]]]]])))
+                                   :value (get-in @test-info [:editing :payload])
+                                   :on-change #(swap! test-info assoc-in  [:editing :payload] (.-target.value %))}]]]]])))
 
-(defn set-editing! [id]
-  (println "EDITING TEST INFO: " (:editing @test-info))
-  (swap! test-info assoc :editing id))
+;; (defn set-editing! [id]
+;;   (println "EDITING TEST INFO: " (:editing @test-info))
+;;   (swap! test-info assoc :editing id))
 
 (defn name-capsule [id]
   [:div.row 
-   [:col-md-10 {:on-click #(set-editing! id)} (str (getName id))]
+   [:col-md-10.test-capsule {:on-click #(set-editing! id)} (str (get-testing-name id))]
    [:col-md-2 [rm-test-btn id]]])
 
 (defn home-page []
@@ -171,11 +168,11 @@
     [:div.jumbotron
      [:h1 "Welcome to API Velociraptor"]]]]
    [:div.row
-    [:h2 {:style {:background-color "tomato"}} (str "TEST INFO: " @test-info " counter: " @counter " EDITING: " (:editing @test-info))]]
+    [:h2 {:style {:background-color "tomato"}} (str "TEST INFO: " @test-info " counter: " @counter " editing: " (:editing @test-info))]]
    [:div.row
-    [:div.col-md-2 {:style {:background-color "blue" :color "white" :font-size "20px"}}
-     (doall (for [test1 @test-info]
-             ^{:key (first test1)} [name-capsule (first test1)]))]
+    [:div.col-md-2 
+     (doall (for [test1 (:testing @test-info)]
+              ^{:key (first test1)} [name-capsule (first test1)]))]
     [:div.col-md-10
      [:h2 "API Info"]
       [api-form]
